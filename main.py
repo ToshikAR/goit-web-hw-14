f3aab8cab7a447687a92c8e137c8c9e30ccc73ef
R.Anatolii <tv.weplay@gmail.com>
2024-11-28 00:09:31 -0800

    hw14.0.1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from pathlib import Path
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
@@ -59,4 +60,10 @@ def root():
 
 
 # if __name__ == "__main__":
-#     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+#     uvicorn.run(
+#         "main:app",
+#         host="0.0.0.0",
+#         port=int(os.environ.get("PORT", 8000)),
+#         reload=True,
+#         log_level="info",
+#     )
